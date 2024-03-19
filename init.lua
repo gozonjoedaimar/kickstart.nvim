241b89336f72d06fc2265e45e1dfe83b87daaca3
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-03-20 06:30:50 +0800

    Autoload gitsign

--- init.lua
+++ init.lua
@@ -307,6 +307,7 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    event = 'BufRead',
     opts = {
       signs = {
         add = { text = '+' },
