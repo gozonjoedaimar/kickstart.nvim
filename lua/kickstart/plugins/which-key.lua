61c088c567bdd3e9e2bf40e47593d989149bc4b5
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-04-12 12:24:40 +0800

    Add filetree

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -29,6 +29,7 @@ return {
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>E', group = 'File [E]xplorer' },
       }
     end,
   },
