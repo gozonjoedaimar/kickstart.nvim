48fe589a4cf3daa06fe6bbd530dd66d80860a924
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-04-13 15:30:39 +0800

    Add copilot

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -22,12 +22,15 @@ return {
 
       -- Document existing key chains
       require('which-key').register {
+        -- Main
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>E'] = { name = 'File [E]xplorer', _ = 'which_key_ignore' },
+        -- Sub
+        ['<leader>cc'] = { name = 'CopilotChat', _ = 'which_key_ignore' },
       }
     end,
   },
