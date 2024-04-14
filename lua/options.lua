4f01b57f63b55f3eba3d82bcf4f3978ce72ace4b
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-04-14 18:31:06 +0800

    Vim default backspace behavior and fileexplorer

--- lua/options.lua
+++ lua/options.lua
@@ -65,10 +65,14 @@ vim.opt.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.opt.confirm = true
+-- Backspace behaviour
+vim.opt.backspace = 'indent,eol,start'
+
 -- Register ejs filetype
 vim.treesitter.language.register('html', { 'ejs' })
-vim.cmd [[
-  au BufRead,BufNewFile *.ejs set filetype=html
-]]
+vim.cmd 'au BufRead,BufNewFile *.ejs set filetype=html'
+
+-- Default vim file explorer list style
+vim.cmd 'let g:netrw_liststyle = 3'
 
 -- vim: ts=2 sts=2 sw=2 et
