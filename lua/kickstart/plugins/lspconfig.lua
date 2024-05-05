0213a56136a1a2c2eb44c3d6a7e8d5a5cb29afbe
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-05-05 12:35:39 +0800

    Add css-lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -203,6 +203,7 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'intelephense',
+        'css-lsp'
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
